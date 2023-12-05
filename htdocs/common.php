bfc8d0cdc1730bec65403a1fbe1bfb9c2f0b93f4
Ivan Dolovčak <ivan.dolovcak@tuta.io>
2023-12-05 17:44:07 +0100

    feat: add PHP config via .htaccess
    - create file "local.lock" - only present locally, not on server
            - if this file exists, PHP errors are displayed
    - add the PHP auto prepend script

---
+++ htdocs/common.php
@@ -0,0 +1,4 @@
+<?php
+# This is the global auto prepend file.
+
+$srvRoot = $_SERVER["DOCUMENT_ROOT"];
---
+++ htdocs/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="hr">
+<head>
+    <meta charset="UTF-8">
+    <title>Exam Engine</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Exam Engine</h1>
+    <hr>
+
+    <p>Dobro došli na početnu stranicu usluge Exam Engine!</p>
+</body>
+</html>
