463b7b2ea03e1ae2d4da02697e663d9039de67c7
Ivan Dolovčak <ivan.dolovcak@tuta.io>
2023-12-09 14:05:11 +0100

    fix: different development/live strategy
    - use environment variable/apache constant for the development
      environment, instead of the "local.lock" file
    - use absolute paths for the auto_prepend_file
    - these are the workarounds for not using a custom php.ini file

--- htdocs/.server/common.php
+++ htdocs/.server/common.php
@@ -3,3 +3,9 @@
 
 declare(strict_types=1);
 set_include_path($_SERVER["DOCUMENT_ROOT"] . "/include");
+
+if ($_SERVER["DEVELOPMENT"] == "true") {
+    error_reporting(E_ALL);
+    ini_set("display_errors", true);
+    ini_set("display_startup_errors", true);
+}
