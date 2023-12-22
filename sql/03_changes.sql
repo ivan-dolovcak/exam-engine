22be7326f3bd5d08e2cb1aa04cc3ce364395b203
Ivan Dolovčak <idolovcak7@gmail.com>
2023-12-22 20:28:25 +0100

    refactor: column renaming

--- sql/03_changes.sql
+++ sql/03_changes.sql
@@ -1,3 +1,7 @@
 use `EXAM_ENGINE`;
 
 rename table `Submittion` to `Submission`;
+
+alter table `Submission` rename column `submittionJson` to `submissionJSON`;
+alter table `Document` rename column `documentJson` to `documentJSON`;
+alter table `Document` rename column `solutionJson` to `solutionJSON`;
