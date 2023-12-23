db2c24affb0aa3949cdd501e018f7b41c93711f9
Ivan Dolovčak <idolovcak7@gmail.com>
2023-12-23 13:51:32 +0100

    feat: add tsconfig; refactor: clean_hosting script

--- htdocs/static/clean_hosting.ts
+++ htdocs/static/clean_hosting.ts
@@ -1,4 +1,11 @@
-const ad: Element | null = document.querySelector("body > div:last-of-type");
-
-if (ad !== null)
+// Remove hosting watermark div in the bottom right corner
+const ad = document.querySelector(
+    "body > div:last-of-type") as HTMLDivElement | null;
+if (ad)
     ad.remove();
+
+// Remove tracking script
+const tracking = document.querySelector(
+    "body > script:last-of-type") as HTMLScriptElement | null;
+if (tracking)
+    tracking.remove();
