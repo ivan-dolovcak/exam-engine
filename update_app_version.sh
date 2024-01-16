77eb5d29da8799b4b7b7a776a75a17c27621ddd1
Ivan Dolovčak <idolovcak7@gmail.com>
2024-01-16 20:24:50 +0100

    refactor: moved update_app_version.sh to local alias

--- update_app_version.sh
+++
@@ -1,8 +0,0 @@
-# Save git commit/tag and date in a file to display on the website
-
-set -e # Exit on error
-
-appVersion="$(git show \
-	--no-patch --format="%(describe) (%ad)" --date="format:%Y-%m-%d %H:%M")"
-
-echo "$appVersion" > htdocs/.app_version
